b1846f6afa74d525b89bb6a9545c8e29f0e4ae34
SanjayB2005 <bsanjay2595@gmail.com>
2025-02-13 19:42:02 +0530

    Remove unused icon imports from the icons index file

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,13 +1,9 @@
-
-
-
 import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
 import hamburger from './hamburger.svg'
 import instagram from './instagram.svg'
 import support from './support.svg'
 import shieldTick from './shield-tick.svg'
-
 import truckFast from './truck-fast.svg'
 import twitter from './twitter.svg'
 import career from './career.png'
@@ -19,16 +15,12 @@ import internship from './internship.png'
 import professionaldevelopment from './professional-development.png'
 
 export {
-    
-   
-   
     copyrightSign,
     facebook,
     hamburger,
     instagram,
     support,
     shieldTick,
-    
     truckFast,
     twitter,
     career,
