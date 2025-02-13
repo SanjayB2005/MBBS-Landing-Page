e6246c9190a4d169b298a294bc0bef207b4d64fc
SanjayB2005 <bsanjay2595@gmail.com>
2025-02-13 19:35:59 +0530

    Clean up icon imports in index.js by removing unused icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,13 +1,13 @@
-import arrowRight from './arrow-right.svg'
-import chevronLeft from './chevron-left.svg'
-import cheveronRight from './chevron-right.svg'
+
+
+
 import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
 import hamburger from './hamburger.svg'
 import instagram from './instagram.svg'
 import support from './support.svg'
 import shieldTick from './shield-tick.svg'
-import star from './star.svg'
+
 import truckFast from './truck-fast.svg'
 import twitter from './twitter.svg'
 import career from './career.png'
@@ -19,16 +19,16 @@ import internship from './internship.png'
 import professionaldevelopment from './professional-development.png'
 
 export {
-    arrowRight,
-    cheveronRight,
-    chevronLeft,
+    
+   
+   
     copyrightSign,
     facebook,
     hamburger,
     instagram,
     support,
     shieldTick,
-    star,
+    
     truckFast,
     twitter,
     career,
